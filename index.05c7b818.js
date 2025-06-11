cb9a84e5d94e6d65022a3594d000fac79e0d82b7
Yurii <j.zherebilov15@gmail.com>
2025-06-11 22:39:57 +0300

    make build

---
+++ index.05c7b818.js
@@ -0,0 +1,2 @@
+!function(e){e.forEach(function(e){var n=document.createElement("label"),t=e.id;n.classList.add("field-label"),n.htmlFor=t,e.name&&(n.textContent=e.name[0].toUpperCase()+e.name.slice(1)),e.placeholder=e.name[0].toUpperCase()+e.name.slice(1),e.parentElement.append(n)})}(document.querySelectorAll("form input"));
+//# sourceMappingURL=index.05c7b818.js.map
